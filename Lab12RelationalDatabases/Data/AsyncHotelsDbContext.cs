0bedf94f251d3e36629336a78d646148f733076c
Sue Tarazi <suemachtley@gmail.com>
2020-04-01 22:39:52 -0700

    Add project files.

---
+++ Lab12RelationalDatabases/Data/AsyncHotelsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12RelationalDatabases.Data
+{
+    public class AsyncHotelsDbContext : DbContext
+    {
+        public AsyncHotelsDbContext(DbContextOptions<AsyncHotelsDbContext> options) : base (options)
+        {
+            
+        }
+
+
+    }
+}
