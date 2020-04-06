6a0405e87f7a12cae61cbd9e081125853447724d
Sue Tarazi <suemachtley@gmail.com>
2020-04-06 12:13:48 -0700

    routes verified :)

--- Lab12RelationalDatabases/Migrations/20200406190505_dbseeding.Designer.cs
+++ Lab12RelationalDatabases/Migrations/20200406190505_dbseeding.Designer.cs
@@ -4,14 +4,16 @@ using Lab12RelationalDatabases.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Lab12RelationalDatabases.Migrations
 {
     [DbContext(typeof(AsyncHotelsDbContext))]
-    partial class AsyncHotelsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200406190505_dbseeding")]
+    partial class dbseeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
