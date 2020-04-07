7ac842a0ba0d93ddb7e20c9ed996274fc84a5206
Sue Tarazi <suemachtley@gmail.com>
2020-04-07 12:49:21 -0700

    RoomsController, IRoomsManager, RoomService and Startup all updated

---
+++ Lab12RelationalDatabases/Models/Interfaces/IRoomsManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12RelationalDatabases.Models.Interfaces
+{
+    public interface IRoomsManager
+    {
+        //create a room
+        Task<Room> CreateRoom(Room room);
+
+        //update a room
+        Task UpdateRoom(int roomId, Room room);
+
+        //get all rooms
+        Task<List<Room>> GetAllRooms();
+
+        //get a single room by ID
+        Task<Room> GetRoomByID(int roomId);
+
+        //delete a room by ID
+        Task RemoveRoom(int roomId);
+
+    }
+}
