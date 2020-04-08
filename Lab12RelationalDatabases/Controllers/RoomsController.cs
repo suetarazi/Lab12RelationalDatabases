1bea36366bbdbf15fe2a50de20085a99c1639f25
Sue Tarazi <suemachtley@gmail.com>
2020-04-08 10:25:01 -0700

    updates to DTO

--- Lab12RelationalDatabases/Controllers/RoomsController.cs
+++ Lab12RelationalDatabases/Controllers/RoomsController.cs
@@ -106,5 +106,6 @@ namespace Lab12RelationalDatabases.Controllers
             return NoContent();
         }
 
+
     }
 }
--- Lab12RelationalDatabases/DTOs/AmenitiesDTO.cs
+++ Lab12RelationalDatabases/DTOs/AmenitiesDTO.cs
@@ -7,8 +7,8 @@ namespace Lab12RelationalDatabases.DTOs
 {
     public class AmenitiesDTO
     {
-        // not incorrect to include ID, but it is not required. It adds more work for the end user to have it
-        //public int ID { get; set; }
+        
+        public int ID { get; set; }
         public string Name { get; set; }
 
     }
