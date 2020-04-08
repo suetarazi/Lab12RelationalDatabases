f115072f40b166fafc29925c8813b01ee58ae0a8
brody-rebne <brody-rebne@BRODYDESK>
2020-04-07 19:05:13 -0700

    created amenities service. refactored amenities controller and interface.

---
+++ Lab12RelationalDatabases/Models/Services/AmenitiesService.cs
@@ -0,0 +1,50 @@
+﻿using Lab12RelationalDatabases.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab12RelationalDatabases.Models.Interfaces;
+
+namespace Lab12RelationalDatabases.Models.Services
+{
+    public class AmenitiesService
+    {
+        private AsyncHotelsDbContext _context;
+        public AmenitiesService(AsyncHotelsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Amenities> CreateAmenities(Amenities amenities)
+        {
+            _context.Amenities.Add(amenities);
+            await _context.SaveChangesAsync();
+            return amenities;
+        }
+
+        public async Task<List<Amenities>> GetAllAmenities()
+        {
+            return await _context.Amenities.ToListAsync();
+        }
+
+        public async Task<Amenities> GetAmenitiesByID(int amenitiesID)
+        {
+            Amenities amenities = await _context.Amenities.FindAsync(amenitiesID);
+            return amenities;
+        }
+
+        public async Task UpdateAmenities(int amenitiesID, Amenities amenities)
+        {
+            _context.Entry(amenities).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAmenities(int amenitiesID)
+        {
+            Amenities amenities = await GetAmenitiesByID(amenitiesID);
+            _context.Amenities.Remove(amenities);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
